6926c01221a41859dfaeada0d8cd2a76bd01c20e
mulqi <mulkioi92@gmail.com>
2023-12-27 21:23:18 +0700

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+  return [
+    {
+      id: 1,
+      name: 'mulqi',
+      tag: 'mulqialkhuatsar',
+      imageUrl: '/images/mulqi.png',
+    },
+    {
+      id: 2,
+      name: 'jajang seher',
+      tag: 'jajangseher',
+      imageUrl: '/images/arifaizin.jpeg',
+    },
+    {
+      id: 3,
+      name: 'ncep knalpot',
+      tag: 'nceppiston',
+      imageUrl: '/images/default.jpg',
+    },
+  ];
+};
+
+export { getData };
